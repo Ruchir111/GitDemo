c515c868c7b2f1e327216866685e2d18e5969564
Ruchir111 <ruchir.patil30@gmail.com>
2026-04-18 15:09:55 +0530

    This is a commit from China at randdom time

--- src/main/java/Resources/ExtentReporterNG.java
+++ src/main/java/Resources/ExtentReporterNG.java
@@ -2,6 +2,8 @@ package Resources;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentReporter;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
 //here when test runs parallel the overiding was happening so
 //we put ThreadLocal Class that makes different thread for 
 //parallel test
@@ -12,7 +14,8 @@ public class ExtentReporterNG {
 		String dynamicPath = System.getProperty("user.dir")+"//reports//index.html";
 		
 		ExtentSparkReporter reporter = new ExtentSparkReporter(dynamicPath);
-		
+		System.out.println("A Guy from china modified the code here");
+		System.out.println("Hi Git Hub Look Who is Back to Learn You");
 		reporter.config().setReportName("WebAutomationsResult");
 		reporter.config().setDocumentTitle("TestResults");
 		
